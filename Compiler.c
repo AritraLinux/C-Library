a7d5dd5573c8519bd3a4c9c09334a5b881a0d999
AritraLinux <37256471+AritraLinux@users.noreply.github.com>
2018-03-10 20:31:55 -0600

    Create Compiler.c

---
+++ Compiler.c
@@ -0,0 +1,16 @@
+# Compiler for the C# Runtime
+# A Project that is an alternative and is a faster version of C# Libraries on Linux
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys.h>
+
+main(){
+
+f.open(compilerfile);
+
+f.read(compilerfiles);
+
+f.read(compilefile);
+
+}
